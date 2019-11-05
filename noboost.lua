659db9a030ad69c640cd96ea2634ac4a7ff2661e
Georg <funer@gmx.at>
2019-11-05 16:41:02 +0100

    filter out more stuff and make it actually work

--- noboost.lua
+++ noboost.lua
@@ -1,11 +1,22 @@
 local wordlist = {
 	"boost",
+	"boosts",
+	"boosting",
 	"verkaufe",
+	"verkauft",
+	"verkaufen",
 	"wts",
 	"vk",
+	"gold",
+	"buy",
+	"sell",
+	"rio",
+	"+rio",
 }
 
-ChatFrame_AddMessageEventFilter("CHAT_MSG_CHANNEL", function(_, _, msg)
+ChatFrame_AddMessageEventFilter("CHAT_MSG_CHANNEL", function(_, _, msg, ...)
 	msg = msg:lower()
-	return (msg:match("boost") or (msg:find("m+") and msg:find(wordlist[msg])))
+	for i = 1, #wordlist do
+		return (msg:find("m+[%d+]") or msg:find("m+") or msg:find("m[%d+]") or msg:find("freehold") or msg:find("freihafen")) and msg:find(wordlist[i])
+	end
 end)
