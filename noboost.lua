5b7d33d72ba055e61eb8851b5b593a1203336f6b
Georg <funer@gmx.at>
2019-11-05 17:18:04 +0100

    add other localized freehold names into a table
    should do for the most part

--- noboost.lua
+++ noboost.lua
@@ -14,9 +14,29 @@ local wordlist = {
 	"+rio",
 }
 
+local freehold = {
+	"Freehold",
+	"Freihafen",
+	"Fuerte Libre",
+	"Port-Liberté",
+	"Covo della Libertà",
+	"Angra do Facão",
+	"Вольная Гавань",
+	"자유지대",
+	"自由镇",
+}
+
 ChatFrame_AddMessageEventFilter("CHAT_MSG_CHANNEL", function(_, _, msg, ...)
 	msg = msg:lower()
 	for i = 1, #wordlist do
-		return (msg:find("m+(%d+)") or msg:find("m+") or msg:find("m(%d+)") or msg:find("freehold") or msg:find("freihafen")) and msg:find(wordlist[i])
+		return (msg:find("m+(%d+)") or msg:find("m+") or msg:find("m(%d+)")) and msg:find(wordlist[i])
+	end
+	-- not so nice, but does it's job 
+	for i = 1, #freehold do
+		if msg:find(freehold[i]) then
+			for i = 1, #wordlist do
+				return msg:find(wordlist[i])
+			end
+		end
 	end
 end)
