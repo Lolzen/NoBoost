01f75ccf2c9c66011a85ca8642b7f3baaabf1727
Georg <funer@gmx.at>
2019-11-05 16:50:09 +0100

    and i stil suck at regex patterns

--- noboost.lua
+++ noboost.lua
@@ -17,6 +17,6 @@ local wordlist = {
 ChatFrame_AddMessageEventFilter("CHAT_MSG_CHANNEL", function(_, _, msg, ...)
 	msg = msg:lower()
 	for i = 1, #wordlist do
-		return (msg:find("m+[%d+]") or msg:find("m+") or msg:find("m[%d+]") or msg:find("freehold") or msg:find("freihafen")) and msg:find(wordlist[i])
+		return (msg:find("m+(%d+)") or msg:find("m+") or msg:find("m(%d+)") or msg:find("freehold") or msg:find("freihafen")) and msg:find(wordlist[i])
 	end
 end)
